35187b8fb0543cd79464fc4eec3e600da9e45860
sourabh sheokand <sourabhsheokand945@gmail.com>
2026-04-27 23:58:50 +0530

    [dev] complete the ipc module

---
+++ Orchestrator/internal/ipc/main.go
@@ -0,0 +1,57 @@
+package ipc
+
+import (
+	"encoding/json"
+	"log/slog"
+	"net"
+
+	// Alias the import to 'typesipc' to avoid colliding with 'package ipc'
+	typesipc "github.com/sourabh945/ForgeQueue/Orchestrator/internal/types"
+)
+
+// connectSendJob connects to a unix socket, sends the job, and returns the connection.
+// NOTE: The caller is responsible for calling conn.Close() when done.
+func connectSendJob(socketPath string, job typesipc.Job, logger *slog.Logger) net.Conn {
+
+	// connecting to the unix socket
+	conn, err := net.Dial("unix", socketPath)
+	if err != nil {
+		logger.Error("Failed to connect to socket", slog.String("error", err.Error()))
+		return nil
+	}
+
+	// making data into json format to send
+	jsonData, err := json.Marshal(job)
+	if err != nil {
+		logger.Error("Failed to marshal JSON", slog.String("error", err.Error()))
+		conn.Close() // Clean up the connection we just opened
+		return nil
+	}
+
+	// writing to socket
+	_, err = conn.Write(jsonData)
+	if err != nil {
+		logger.Error("Failed to write to socket", slog.String("error", err.Error()))
+		conn.Close()
+		return nil
+	}
+
+	return conn
+}
+
+// waitForJobResponse waits for a job response from the socket and returns it.
+// It closes the connection at the end.
+func waitForJobResponse(conn net.Conn, logger *slog.Logger) typesipc.JobResponse {
+
+	// closing the connection at end
+	defer conn.Close()
+
+	// reading from socket
+	decoder := json.NewDecoder(conn)
+	var response typesipc.JobResponse
+	if err := decoder.Decode(&response); err != nil {
+		logger.Error("Failed to decode response", slog.String("error", err.Error()))
+		return typesipc.JobResponse{}
+	}
+	return response
+}
--- Orchestrator/internal/types/ipc.go
+++ Orchestrator/internal/types/ipc.go
@@ -5,3 +5,9 @@ type Job struct {
 	ImagePath string `json:"ImagePath"`
 	Type      string `json:"Type"`
 }
+
+type JobResponse struct {
+	JobId     string `json:"JobId"`
+	ImagePath string `json:"ImagePath"`
+	Status    string `json:"Status"`
+}
